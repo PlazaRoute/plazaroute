786723c0dc10ed18f8d599e7578b5c873b724523
Jonas Matter <jonas.matter@bluewin.ch>
2017-10-29 16:09:32 +0100

    add seachch test-script

---
+++ routing-tests/search-ch-api/search-ch-test.py
@@ -0,0 +1,20 @@
+import requests
+import sys
+
+this = sys.modules[__name__]
+this.search_ch_url = 'https://timetable.search.ch/api/route.json'
+
+
+def get_timetable(start, destination):
+    payload = {'from': start, 'to': destination}
+    req = requests.get(this.search_ch_url, params=payload)
+
+    print(req.url)
+    response = req.json()
+    print(response)
+
+
+if __name__ == '__main__':
+    start = 'Sternen Oerlikon'
+    destination = 'Hallenband Oerlikon'
+    get_timetable(start, destination)
