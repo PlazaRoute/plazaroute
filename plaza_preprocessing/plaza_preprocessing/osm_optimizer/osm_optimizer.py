3dab9d76f2f4416be1bee0bb5c0b89042e736de8
Robin Suter <suter.robin@gmail.com>
2017-10-31 14:17:45 +0100

    prepare plaza for graph

--- plaza_preprocessing/plaza_preprocessing/osm_optimizer/osm_optimizer.py
+++ plaza_preprocessing/plaza_preprocessing/osm_optimizer/osm_optimizer.py
@@ -1,26 +1,30 @@
 from plaza_preprocessing import osm_importer as importer
 from plaza_preprocessing.osm_merger import geojson_writer
-from shapely.geometry import Point, MultiPoint, LineString, MultiLineString, Polygon, MultiPolygon
+from shapely.geometry import (Point, MultiPoint, LineString, MultiLineString,
+                              Polygon, MultiPolygon, box)
 
 
 def preprocess_plazas(osm_holder):
     """ preprocess all plazas from osm_importer """
     # test for helvetiaplatz
     # plaza = next(p for p in osm_holder.plazas if p['osm_id'] == 4533221)
-    # process_plaza(plaza, osm_holder.lines, osm_holder.buildings)
+    # test for Bahnhofplatz Bern
+    # plaza = next(p for p in osm_holder.plazas if p['osm_id'] == 5117701)
+    # process_plaza(plaza, osm_holder.lines, osm_holder.buildings, osm_holder.points)
     for plaza in osm_holder.plazas:
-        process_plaza(plaza, osm_holder.lines, osm_holder.buildings)
+        process_plaza(plaza, osm_holder.lines, osm_holder.buildings, osm_holder.points)
 
 
-def process_plaza(plaza, lines, buildings):
+def process_plaza(plaza, lines, buildings, points):
     """ process a single plaza """
     print(f"processing plaza {plaza['osm_id']}")
     entry_points = calc_entry_points(plaza['geometry'], lines)
     if not entry_points:
         print(f"Plaza {plaza['osm_id']} has no entry points")
         return
-    create_obstacle_geometry(plaza['geometry'], buildings)
-    
+    plaza['geometry'] = insert_obstacles(plaza['geometry'], buildings, points)
+
+    geojson_writer.write_geojson([plaza['geometry']], 'plaza.geojson')
 
 
 def calc_entry_points(plaza_geometry, lines):
@@ -28,7 +32,7 @@ def calc_entry_points(plaza_geometry, lines):
     calculate points where lines intersect with the outer ring of the plaza
     """
     intersecting_lines = find_intersescting_lines(plaza_geometry, lines)
-    
+
     entry_points = []
     for line in intersecting_lines:
         intersection = line.intersection(plaza_geometry)
@@ -53,17 +57,26 @@ def calc_entry_points(plaza_geometry, lines):
 
 def find_intersescting_lines(plaza_geometry, lines):
     """ return every line that intersects with the plaza """
-    # filtering is slower than checking every line 
+    # filtering is slower than checking every line
     # bbox_buffer = 5 * 10**-3  # about 500m
-    # lines_in_approx = list(filter(lambda l: line_in_plaza_bbox(l, plaza_geometry, buffer=bbox_buffer), lines))
+    # lines_in_approx = list(
+    #     filter(lambda l: line_in_plaza_approx(l, plaza_geometry, buffer=bbox_buffer), lines))
     return list(filter(plaza_geometry.intersects, lines))
 
 
-def create_obstacle_geometry(plaza_geometry, buildings):
-    """ create a geometry that contains every obstacle on the plaza """
+def insert_obstacles(plaza_geometry, buildings, points):
+    """ cuts out holes for obstacles on the plaza geometry """
     intersecting_buildings = find_intersecting_buildings(plaza_geometry, buildings)
-    geojson_writer.write_geojson(intersecting_buildings, 'buildings.geojson')
-    # TODO: Add point cutouts
+
+    for building in intersecting_buildings:
+        plaza_geometry = plaza_geometry.difference(building)
+
+    points_on_plaza = get_points_inside_plaza(plaza_geometry, points)
+    point_obstacles = list(map(lambda p: create_point_obstacle(p, buffer=2), points_on_plaza))
+
+    for p_obstacle in point_obstacles:
+        plaza_geometry = plaza_geometry.difference(p_obstacle)
+    return plaza_geometry
 
 
 def find_intersecting_buildings(plaza_geometry, buildings):
@@ -71,21 +84,50 @@ def find_intersecting_buildings(plaza_geometry, buildings):
     return list(filter(plaza_geometry.intersects, buildings))
 
 
+def get_points_inside_plaza(plaza_geometry, points):
+    """ finds all points that are on the plaza geometry """
+    return list(filter(plaza_geometry.intersects, points))
 
-def line_in_plaza_bbox(line, plaza_geometry, buffer=0):
+
+def create_point_obstacle(point, buffer=5):
+    """ create a polygon around a point with a buffer in meters """
+    buffer_deg = meters_to_degrees(buffer)
+    min_x = point.x - buffer_deg
+    min_y = point.y - buffer_deg
+    max_x = point.x + buffer_deg
+    max_y = point.y + buffer_deg
+    return box(min_x, min_y, max_x, max_y)
+
+
+def create_visibility_graph(plaza_geometry, entry_points):
+    """ create a visibility graph with all plaza and entry points """
+    pass
+
+
+def line_in_plaza_approx(line, plaza_geometry, buffer=0):
     """
-    determines if any node of a line is in the bounding box of the plaza,
+    determines if a line's bounding box is in the bounding box of the plaza,
     with optional buffer in degrees (enlarged bounding box)
     """
-    min_x, min_y, max_x, max_y = plaza_geometry.bounds
-    min_x -= buffer / 2
-    min_y -= buffer / 2
-    max_x += buffer / 2
-    max_y += buffer / 2
-    for p in line.coords:
-        if point_in_bounding_box(Point(p), min_x, min_y, max_x, max_y):
-            return True
-    return False
+    min_x1, min_y1, max_x1, max_y1 = plaza_geometry.bounds
+    line_bbox = line.bounds
+    min_x1 -= buffer / 2
+    min_y1 -= buffer / 2
+    max_x1 += buffer / 2
+    max_y1 += buffer / 2
+    return bounding_boxes_overlap(min_x1, min_y1, max_x1, max_y1, *line_bbox)
+
+
+def bounding_boxes_overlap(min_x1, min_y1, max_x1, max_y1, min_x2, min_y2, max_x2, max_y2):
+    """ takes two bounding boxes and checks if they overlap """
+    if min_x1 <= min_x2 and max_x1 <= min_x2:
+        return False
+    if min_y1 <= min_y2 and max_y1 <= min_y2:
+        return False
+    if min_x1 >= max_x2 or min_y1 >= max_y2:
+        return False
+
+    return True
 
 
 def point_in_plaza_bbox(point, plaza_geometry):
@@ -101,7 +143,15 @@ def point_in_bounding_box(point, min_x, min_y, max_x, max_y):
         return False
     return True
 
+
+def meters_to_degrees(meters):
+    """ convert meters to approximate degrees """
+    #  meters * 360 / (2 * PI * 6400000)
+    # multiply by (1/cos(lat) for longitude)
+    return meters * 1/111701
+
+
 if __name__ == '__main__':
-    osm_holder = importer.import_osm('data/helvetiaplatz_umfeld.osm')
-    # osm_holder = importer.import_osm('data/switzerland-exact.osm.pbf')
+    # osm_holder = importer.import_osm('data/helvetiaplatz_umfeld.osm')
+    osm_holder = importer.import_osm('data/switzerland-exact.osm.pbf')
     preprocess_plazas(osm_holder)
