eb93b1c30ef5ba7a3d198f44ab8aceb463115b3b
Jonas Matter <jonas.matter@bluewin.ch>
2017-11-04 12:23:07 +0100

    add response model

--- plaza_routing/plaza_routing/plaza_routing.py
+++ plaza_routing/plaza_routing/plaza_routing.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restplus import Resource, Api, reqparse
+from flask_restplus import Resource, Api, reqparse, fields
 
 app = Flask(__name__)
 api = Api(app,
@@ -14,11 +14,16 @@ routing_arguments.add_argument('start', type=str, required=True, help='Start loc
 routing_arguments.add_argument('destination', type=str, required=True, help='Destination address')
 
 
+routing_response_model = api.model('RoutingResponse', {
+    'coordinates': fields.List(fields.List(fields.Float)),
+})
+
+
 @ns.route('/route')
 class PlazaRouting(Resource):
 
     @ns.expect(routing_arguments, validate=True)
-    @api.response(200, 'Route successfully retrieved.')
+    @api.response(200, 'Route successfully retrieved.', routing_response_model)
     def get(self):
         args = routing_arguments.parse_args()
         return route(args.get('start'), args.get('destination'))
