e6b76f85722d976ace2e40c95c1c9abbc295cde4
Robin Suter <suter.robin@gmail.com>
2017-11-09 09:11:55 +0100

    add missing utils files

---
+++ plaza_preprocessing/tests/utils.py
@@ -0,0 +1,15 @@
+import testfilemanager
+from plaza_preprocessing.osm_optimizer import osm_optimizer
+
+
+def process_plaza(testfile, plaza_id, process_strategy):
+    holder = testfilemanager.import_testfile(testfile)
+    plaza = get_plaza_by_id(holder.plazas, plaza_id)
+    processor = osm_optimizer.PlazaPreprocessor(holder, process_strategy)
+    return processor.process_plaza(plaza)
+
+
+def get_plaza_by_id(plazas, osm_id):
+    plaza = list(filter(lambda p: p['osm_id'] == osm_id, plazas))
+    assert len(plaza) == 1
+    return plaza[0]
