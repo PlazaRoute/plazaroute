e9c25df233e21786f361d91ce59ae66e994faa80
Jonas Matter <jonas.matter@bluewin.ch>
2017-11-11 12:13:07 +0100

    move tests to correct subfolders

--- plaza_routing/tests/integration/test_search_ch_parser.py
+++ plaza_routing/tests/integration/test_search_ch_parser.py
@@ -8,7 +8,7 @@ from plaza_routing.integration.util import search_ch_parser
 def test_parse_connections():
     search_ch_response_path = os.path.dirname(__file__)
     search_ch_response_file = os.path.join(search_ch_response_path,
-                                           'resources/search_ch_response.json')
+                                           '../resources/search_ch_response.json')
     search_ch_response = open(search_ch_response_file, 'r').read()
     search_ch_parser.parse_connections(search_ch_response)
 
@@ -22,6 +22,6 @@ def test_parse_connections_invalid_response():
     with pytest.raises(RuntimeError):
         search_ch_response_path = os.path.dirname(__file__)
         search_ch_response_file = os.path.join(search_ch_response_path,
-                                               'resources/search_ch_invalid_response.json')
+                                               '../resources/search_ch_invalid_response.json')
         search_ch_response = open(search_ch_response_file, 'r').read()
         search_ch_parser.parse_connections(search_ch_response)
