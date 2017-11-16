65e2c53c033b05c9563107faad8ecb164e059335
Jonas Matter <jonas.matter@bluewin.ch>
2017-11-16 09:39:29 +0100

    fix pep warnings

--- plaza_routing/plaza_routing/integration/routing_strategy/graphhopper_strategy.py
+++ plaza_routing/plaza_routing/integration/routing_strategy/graphhopper_strategy.py
@@ -14,16 +14,17 @@ class GraphHopperRoutingStrategy(RoutingStrategy):
         self._client = SwaggerClient.from_url(f'file://{swagger_file}')
 
     def route(self, start, destination):
-        response = self._client.Routing.get_route(point=[f'{start[0]},{start[1]}', f'{destination[0]},{destination[1]}'],
-                                                  vehicle='foot',
-                                                  points_encoded=False,
-                                                  instructions=False,
-                                                  key='').result()
+        response = self._client.Routing.get_route(
+            point=[f'{start[0]},{start[1]}', f'{destination[0]},{destination[1]}'],
+            vehicle='foot',
+            points_encoded=False,
+            instructions=False,
+            key='').result()
         first_path = response.paths[0]
 
         return {'type': 'walking',
                 'time': first_path.time / 1000,  # convert time to seconds
                 'ascend': first_path.ascend,
                 'descend': first_path.descend,
-                'path': first_path.points.coordinates,
-               }
+                'path': first_path.points.coordinates
+                }
