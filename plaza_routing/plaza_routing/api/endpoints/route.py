253e8b70cb887c0f9c53f04fbb4035c70bec3536
Robin Suter <suter.robin@gmail.com>
2017-11-17 09:51:15 +0100

    add logging to plaza_routing

--- plaza_routing/plaza_routing/api/endpoints/route.py
+++ plaza_routing/plaza_routing/api/endpoints/route.py
@@ -1,8 +1,10 @@
+import logging
 from flask_restplus import Resource, reqparse, fields
 
 from plaza_routing.api.restplus import api
 from plaza_routing.business.plaza_route_service import route
 
+logger = logging.getLogger('plaza_routing')
 ns = api.namespace('route', description='Routing operations')
 
 routing_arguments = reqparse.RequestParser()
@@ -22,4 +24,7 @@ class PlazaRouting(Resource):
     @api.response(200, 'Route successfully retrieved.', routing_response_model)
     def get(self):
         args = routing_arguments.parse_args()
-        return route(args.get('start'), args.get('destination'))
+        start = args.get('start')
+        destination = args.get('destination')
+        logger.debug("Calling route() with start='%s', destination='%s'", start, destination)
+        return route(start, destination)
