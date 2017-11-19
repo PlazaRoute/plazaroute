39064eccb146b0c0ff7634cb6795cbd2c28f1e01
Jonas Matter <jonas.matter@bluewin.ch>
2017-11-19 12:31:03 +0100

    add type hinting to search ch service

--- plaza_routing/plaza_routing/integration/search_ch_service.py
+++ plaza_routing/plaza_routing/integration/search_ch_service.py
@@ -6,14 +6,14 @@ SEARCH_CH_API_URL = 'https://timetable.search.ch/api/route.json'
 TIME_REGEX = re.compile(r'^(([01]\d|2[0-3]):([0-5]\d)|24:00)$')
 
 
-def _validate_arguments(start, destination, time):
+def _validate_arguments(start: str, destination: str, time: str):
     if not start or not destination or not time:
         raise ValueError('empty arguments are not allowed')
     if not TIME_REGEX.match(time):
         raise ValueError('invalid time format: %H:%M')
 
 
-def get_connection(start, destination, time):
+def get_connection(start: str, destination: str, time: str) -> dict:
     """ retrieves the connection for a given start, destination and time of departure"""
     _validate_arguments(start, destination, time)
     payload = {'from': start, 'to': destination, 'time': time, 'num': 1}
