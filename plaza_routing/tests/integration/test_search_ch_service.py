ab7e01473f853a110e45a5b9604dbb71cb9460ec
Jonas Matter <jonas.matter@bluewin.ch>
2017-11-29 20:49:51 +0100

    remove tests that check invalid params in the service layer, cause business layer is a error barricade

--- plaza_routing/tests/integration/test_search_ch_service.py
+++ plaza_routing/tests/integration/test_search_ch_service.py
@@ -14,18 +14,6 @@ def test_get_connection():
     assert connection['number_of_legs'] == 1
 
 
-def test_get_connection_empty_input():
-    with pytest.raises(ValueError):
-        search_ch_service.get_connection('', '', '')
-
-
-def test_get_connection_invalid_time():
-    with pytest.raises(ValueError):
-        search_ch_service.get_connection('Zürich, Sternen Oerlikon',
-                                         'Zürich, Messe/Hallenstadion',
-                                         '07/00')
-
-
 def test_get_connection_same_start_and_destination():
     with pytest.raises(RuntimeError):
         search_ch_service.get_connection('Zürich, Hallenstadionn',
