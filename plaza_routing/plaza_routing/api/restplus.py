34c2f0113896f3b17f0e3e087fbb8eef3c841693
Jonas Matter <jonas.matter@bluewin.ch>
2017-12-09 13:37:25 +0100

    add model example values and possible responses

--- plaza_routing/plaza_routing/api/restplus.py
+++ plaza_routing/plaza_routing/api/restplus.py
@@ -9,9 +9,14 @@ api = Api(version='1.0', title='PlazaRouting API', description='PlazaRouting API
 logger = logging.getLogger('plaza_routing')
 
 
+@api.errorhandler(ValueError)
+def test_error_handler(e):
+    logger.error('test error')
+    return {'message': 'test error'}
+
 @api.errorhandler
 def default_error_handler(e):
-    message = f'An unhandled exception occurred: {e}'
+    message = f'An exception occurred: {e}'
     logger.error(message)
     if not FLASK_DEBUG:
         return {'message': message}, 500
