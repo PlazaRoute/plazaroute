e085c4fdaed68cffccbb620e3e38dda496a8d372
Jonas Matter <jonas.matter@bluewin.ch>
2017-12-09 16:06:57 +0100

    add search.ch tests to test new exception handling

--- plaza_routing/plaza_routing/integration/search_ch_service.py
+++ plaza_routing/plaza_routing/integration/search_ch_service.py
@@ -3,13 +3,14 @@ import requests
 
 from plaza_routing import config
 from plaza_routing.integration.util import search_ch_parser
-from plaza_routing.integration.util.exception_util import ServiceError
+from plaza_routing.integration.util.exception_util import ServiceError, ValidationError
 
 logger = logging.getLogger('plaza_routing.search_ch_service')
 
 
 def get_connection(start: str, destination: str, time: str) -> dict:
     """ retrieves the connection for a given start, destination and time of departure"""
+    req = None
     try:
         payload = {'from': start, 'to': destination, 'time': time, 'num': 1}
         req = requests.get(config.search_ch['search_ch_api'], params=payload)
@@ -17,6 +18,12 @@ def get_connection(start: str, destination: str, time: str) -> dict:
         first_connection = connections['connections'][0]
         return first_connection
     except Exception as exception:
-        msg = f'search.ch is not running correctly: {exception}'
-        logger.error(msg)
-        raise ServiceError(msg) from None
+        _parse_exception(exception, req)
+
+
+def _parse_exception(exception: Exception, req):
+    if req and "Start- und Zielort müssen sich unterscheiden" in req.text:
+        raise ValidationError('start and destination should differ') from None
+    msg = f'search.ch is not running correctly: {exception}'
+    logger.error(msg)
+    raise ServiceError(msg) from None
--- plaza_routing/tests/integration/util/mock_config.py
+++ plaza_routing/tests/integration/util/mock_config.py
@@ -15,6 +15,20 @@ def mock_geocoding_wrong_url(monkeypatch):
                                             "https://overpass.osm.ch/api/interpreter"))
 
 
+def mock_search_ch_unavailable_url(monkeypatch):
+    monkeypatch.setattr(config, "search_ch",
+                        _mock_value_in_dict(config.search_ch,
+                                            "search_ch_api",
+                                            "https://timetable.offline.search.ch/api/route.json"))
+
+
+def mock_search_ch_wrong_url(monkeypatch):
+    monkeypatch.setattr(config, "search_ch",
+                        _mock_value_in_dict(config.search_ch,
+                                            "search_ch_api",
+                                            "https://overpass.osm.ch/api/interpreter"))
+
+
 def _mock_value_in_dict(old_dict: dict, key: str, new_value) -> dict:
     dict_copy = dict(old_dict)
     dict_copy[key] = new_value
