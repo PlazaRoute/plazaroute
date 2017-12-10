4eea2acb32b6673519ad5138d5994a7cff67cdf7
Jonas Matter <jonas.matter@bluewin.ch>
2017-12-10 12:57:57 +0100

    add date parameter to search.ch call to make the tests more stable

--- plaza_routing/plaza_routing/integration/search_ch_service.py
+++ plaza_routing/plaza_routing/integration/search_ch_service.py
@@ -3,9 +3,9 @@ from plaza_routing.integration.util import search_ch_parser
 from plaza_routing import config
 
 
-def get_connection(start: str, destination: str, time: str) -> dict:
+def get_connection(start: str, destination: str, time: str, date='today') -> dict:
     """ retrieves the connection for a given start, destination and time of departure"""
-    payload = {'from': start, 'to': destination, 'time': time, 'num': 1}
+    payload = {'from': start, 'to': destination, 'time': time, 'date': date, 'num': 1}
     req = requests.get(config.search_ch['search_ch_api'], params=payload)
     connections = search_ch_parser.parse_connections(req.text)
     first_connection = connections['connections'][0]
