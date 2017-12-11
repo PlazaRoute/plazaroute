8d6c2eeccc5cb779be90b4a9bf14e5cf1b1d4d84
Jonas Matter <jonas.matter@bluewin.ch>
2017-12-11 13:49:23 +0100

    move routing-tests to newly created repo PlazaRoute-routing-algorithms

--- routing-tests/routing-engines/graphhopper/client.py
+++
@@ -1,6 +0,0 @@
-from bravado.client import SwaggerClient
-client = SwaggerClient.from_url('file:///opt/dev/git/PlazaNav/PlazaNav/routing-tests/graphhopper/swagger.json')
-result = client.Routing.get_route(point=['47.366353,8.544976', '47.365888,8.54709'], vehicle='foot', points_encoded=False, key='', instructions=False).result()
-
-for path in result.paths:
-    print(path.points.coordinates)
